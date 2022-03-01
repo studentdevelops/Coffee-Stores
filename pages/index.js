bda99e441f8b5c0b9cfd92c4bd706e577a1f1fd6
Sourav Kumar <31382363+studentdevelops@users.noreply.github.com>
2022-03-01 09:46:29 +0530

    finals

--- pages/index.js
+++ pages/index.js
@@ -79,12 +79,12 @@ export default function Home(initialProps) {
         <meta property="og:url" content="https://metatags.io/" />
         <meta property="og:title" content="Coffee Connoisseur" />
         <meta property="og:description" content="Come and Find Your Coffee Store to make it your every Adda. Explore Coffee Stores Near you." />
-        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+        <meta property="og:image" content="/metaImage.jpg" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://metatags.io/" />
         <meta property="twitter:title" content="Coffee Connoisseur" />
         <meta property="twitter:description" content="Come and Find Your Coffee Store to make it your every Adda. Explore Coffee Stores Near you." />
-        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+        <meta property="twitter:image" content="/metaImage.jpg" />
       </Head>
       <Banner LocationErrorMsg={LocationErrorMsg} coffeeFetchError={coffeeFetchError} ButtonText={FindingLocation ? "Locating" : "View stores nearby"} onClickFind={onClickFind} />
       <div className={styles.bannerImage}>
